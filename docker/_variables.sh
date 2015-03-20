98f64acdd23abb8835d151633e241712f56df849
Leonardo Shinagawa <leonardo@ebussola.com>
2015-03-19 21:19:39 -0300

    Update _variables.sh
    added missing comments

--- docker/_variables.sh
+++ docker/_variables.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # The project name
+# Use only lowercase, no hyphens, no spaces and no special chars
 octoberProjectName="example"
 
 # plugins paths that have composer to be installed
