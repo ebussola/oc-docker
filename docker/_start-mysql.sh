175a66cc864fbbad6762337541b1fca8965a6347
Leonardo Shinagawa <leonardo@ebussola.com>
2015-04-24 14:45:44 -0300

    Update _start-mysql.sh

--- docker/_start-mysql.sh
+++ docker/_start-mysql.sh
@@ -3,6 +3,7 @@ cd "`dirname "$0"`"
 source _variables.sh
 
 if ! docker ps | grep -q ${octoberProjectName}-mysql; then
+    docker build -t="dockerfile/mysql" github.com/dockerfile/mysql
     docker run -d --name ${octoberProjectName}-mysql dockerfile/mysql
     sleep 2
     ./run.sh mysql -h ${octoberProjectName}-mysql -e "create database ${octoberProjectName}"
